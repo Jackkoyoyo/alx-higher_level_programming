783a013f8564c4f39b8266d74046c4873f0b0b15
Jackkoyoyo <Jackkoyoyo@gmail.com>
2024-05-22 20:42:20 -0700

    Commit 4

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,9 @@
+-- Insert some data into table 'second_table'
+-- Names, John, Alex, Bob and George
+-- Id 1,2,3,4  score, 10,3,14,8
+INSERT INTO second_table (id, name, score) VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8)
+;
