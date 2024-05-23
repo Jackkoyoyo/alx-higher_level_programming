7b919631cdb79b34cd2e3e830d9b47d908e74f7f
Jackkoyoyo <Jackkoyoyo@gmail.com>
2024-05-22 21:12:04 -0700

    Last mandatory commit

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,3 +1,13 @@
+-- Display the number of records in the table 'first_table' where id = 89
+SELECT COUNT(*) FROM first_table WHERE id = 89;
+
+-- Create a new table 'second_table'
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(255),
+    score INT
+);
+
 -- Insert some data into table 'second_table'
 -- Names, John, Alex, Bob and George
 -- Id 1,2,3,4  score, 10,3,14,8
