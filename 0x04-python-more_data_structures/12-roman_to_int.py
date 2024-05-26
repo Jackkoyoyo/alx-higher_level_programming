6d21cd0b8b5b980c17fbedebc1572e01dc01e148
Jackkoyoyo <Jackkoyoyo@gmail.com>
2024-05-26 00:47:49 -0700

    Correction

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,7 +5,7 @@ def roman_to_int(roman_string):
     roman_conv = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     result, pre_val = 0, 0
     for r in roman_string:
-        value = roman_d[r]
+        value = roman_conv[r]
         result += value
         if value > prev_val:
             result -= prev_val * 2
