e3db0e0a62e47683a1993338f5fea376a5474468
Jackkoyoyo <Jackkoyoyo@gmail.com>
2024-05-26 00:41:41 -0700

    Last commit mandatory

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    roman_conv = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    result, pre_val = 0, 0
+    for r in roman_string:
+        value = roman_d[r]
+        result += value
+        if value > prev_val:
+            result -= prev_val * 2
+        prev_val = value
+    return result
