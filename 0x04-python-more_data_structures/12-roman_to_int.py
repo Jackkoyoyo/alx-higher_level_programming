e791db9b94430a53d33f3c245bf994b20d3e1a7c
Jackkoyoyo <Jackkoyoyo@gmail.com>
2024-05-26 01:10:22 -0700

    Correction 2

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,11 +3,11 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
     roman_conv = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    result, pre_val = 0, 0
-    for r in roman_string:
-        value = roman_conv[r]
+    result, prev_value = 0, 0
+    for c in roman_string:
+        value = roman_conv[c]
         result += value
-        if value > prev_val:
-            result -= prev_val * 2
-        prev_val = value
+        if value > prev_value:
+            result -= prev_value * 2
+        prev_value = value
     return result
