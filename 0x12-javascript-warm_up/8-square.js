97fd6ee0a6d7ebed77b8badcfa0f514413040974
Jackkoyoyo <Jackkoyoyo@gmail.com>
2024-06-20 18:34:55 -0700

    Js third

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const squar = process.argv[2];
+if (isNaN(squar)) {
+  console.log('Missing size');
+} else {
+  for (let x = 0; x < sqr; x++) {
+    console.log('X'.repeat(squar));
+  }
+}
