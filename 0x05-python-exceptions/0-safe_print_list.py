f4a60558e9a6c9db6ad89d43543d9fc7c9de956d
Jackkoyoyo <Jackkoyoyo@gmail.com>
2025-05-04 08:31:54 +0000

    Im here!

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+def safe_print_list(my_list=[], x=0):
+    ncount = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            ncount += 1
+    except IndexError:
+        pass
+    print()  # Print a new line after the loop
+    return ncount
